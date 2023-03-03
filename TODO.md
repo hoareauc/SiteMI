19345f631d9a2c646d0137086de566c6eb024a71
L2 <luclaurent@crans.org>
2023-03-03 17:54:01 +0100

    add new files + TODO list + many corrections

---
+++ TODO.md
@@ -0,0 +1,14 @@
+* RESPONSIVITE sur smartphone
+* petites descriptions des cours
+* description stages (optionel en M1 et obligatoire en M2 6 mois)
+* scolarships?? https://master.roc.cnam.fr/index.php/scholarships/
+* FAQ? https://master.roc.cnam.fr/index.php/faq/
+* brochure d'accueil
+* petite gallerie
+* reglement des études??
+* plan du Cnam StMartin/Conté https://master.roc.cnam.fr/index.php/planning/
+* lien campus france https://taughtie.campusfrance.org/tiesearch/#/program/2761
+* dates? deadline + précises ??
+* détails info admission? https://master.roc.cnam.fr/index.php/admission/
+* info frais? https://master.roc.cnam.fr/index.php/tuition-fees/
+* photos: https://master.roc.cnam.fr/index.php/welcome-to-the-2022-2023-cohort/
---
+++ highlights.js
@@ -0,0 +1,33 @@
+document.write('\
+\
+<div id="content1"><center><b>Calendar</b></center>\
+<ul>\
+    <li>Registration: till end of June</li>\
+    <li>Visa: till end of July</li>\
+    <li>Arrival: till end of September</li>\
+</ul>\
+<ul>\
+    <li>Start of classes: October</li>\
+    <li>End of classes: June</li>\
+</ul>	\
+</div>\
+<div id="content2"><center><b>Fees</b></center>\
+<ul>\
+    <li><b>3770 € per year</b></li>\
+    <li>Your employer or a sponsoring company can cover the registration fees.</li>\
+    <li>Scholarships covering tuition fees and/or living expenses can be attributed to \
+outstanding students.</li>\
+</ul>\
+</div>\
+\
+<div id="content3">\
+<center><b>Contact</b></center>\
+<ul>\
+    <li><a style="color:rgb(255, 255, 255);" href="mailto:international@lecnam.net?subject=[MASTER-MECHANICS]">Professor Jean-François Deü</a></li>\
+    <li><a style="color:rgb(255, 255, 255);"href="mailto:international@lecnam.net?subject=[MASTER-MECHANICS]">Professor Luc Laurent</a></li>\
+</ul>\
+    <ul>\
+    <li>international@lecnam.net</li>\
+</ul>\
+</div>\
+');
